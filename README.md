1fbcb10fa203a17d171d973852ab27764502446b
Silohon ROG <akbarsilohon@gmail.com>
2026-06-13 00:51:30 +0700

    Project page is Done

--- README.md
+++ README.md
@@ -1,37 +1,9 @@
 ## Noted Template
 
-### Home Page
-- Hero (Done)
-- News (Done)
-- Visi & Misi (Done)
-- Struktur pengurus (Done)
-- Galeri (Done)
-- Program kerja (Done)
-- FAQs (Done)
-
 ### Backen and CRUD
 - Hero
-- News (Done)
-- Visi & Misi (Done)
-- Struktur pengurus (Done)
-- Galeri (Done)
-- Program kerja (Done)
-- FAQs (Done)
 
 ### Frontend
-- Posts
--- Single Post (Done)
--- Post Category (Done)
--- Post Tag (Done)
-
-- Page
--- Normal Page
--- News (Done)
-
--- Pengurus
---- Looping pengurus (Done)
---- Single View Pengurus (Done)
-
 -- Galeri
 --- Looping Galeri
 --- Single View Galeri
--- page.php
+++ page.php
@@ -1,9 +1,33 @@
 <?php get_header(); ?>
 
+<div class="single_spacer"></div>
 <div class="container">
-    <br><br><br><br><br><br><br><br><br>
-    <h1 class="hero_heading">Proses Develop</h1>
-    <br><br><br><br><br><br><br><br><br>
+    <div class="page_normal-box">
+        <div class="ansor_single-post">
+            <div class="single_breadchumb">
+                <a href="<?php echo home_url(); ?>" class="bread_url">Home</a>
+                <span class="sparator_single">
+                    <i class="fa-solid fa-caret-right"></i>
+                </span>
+                <a href="<?php the_permalink(); ?>" class="bread_url"><?php the_title(); ?></a>
+            </div>
+
+            <?php the_title('<h1 class="single-post-title">', '</h1>'); ?>
+
+            <?php if ( has_post_thumbnail() ) { ?>
+                <figure class="single_post-thumbnail">
+                    <?php the_post_thumbnail( 'full', array(
+                        'class'   => 'single_post-thumbnail-img',
+                        'loading' => 'eager'
+                    )); ?>
+                </figure>
+            <?php } ?>
+
+            <article id="post-<?php the_ID(); ?>" class="single_post-body">
+                <?php the_content(); ?>
+            </article>
+        </div>
+    </div>
 </div>
 
 <?php get_footer();
